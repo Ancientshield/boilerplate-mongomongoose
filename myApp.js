4f9ab13de30f8065bf76bdba4d59dd02b73eb945
Jack Lin <jack_g1341@goodwill.coml.tw>
2023-12-08 10:44:32 +0800

    finished 2. Create a Model

--- myApp.js
+++ myApp.js
@@ -7,7 +7,15 @@ mongoose.connect(process.env.MONGO_URI, {
 	useUnifiedTopology: true,
 });
 
-let Person;
+// 2. Create a Model
+const Schema = mongoose.Schema;
+const personSchema = new Schema({
+	name: { type: String, required: true },
+	age: Number,
+	favoriteFoods: [String],
+});
+
+const Person = mongoose.model('Person', personSchema);
 
 const createAndSavePerson = (done) => {
 	done(null /*, data*/);
